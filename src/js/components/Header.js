cf274dc65aa1525971b57d3042b2f9f9f59a748f
axatrikx <axatrikx@gmail.com>
2016-12-04 18:00:47 +0530

    added navbar

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -5,11 +5,24 @@ export default class Header extends React.Component {
 
     render() {
         return (
-            <div>
-                <Link to="/" className="btn btn-primary">Home</Link>
-                <Link to="settings" className="btn btn-default">Settings</Link>
-                <Link to="about" className="btn btn-default">About</Link>
-            </div>
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <li><Link to="/" className="navbar-brand" >Axa Media</Link></li>
+                    </div>
+                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <form className="navbar-form navbar-left">
+                        <div className="form-group">
+                        <input type="text" className="form-control" placeholder="Search"/>
+                        </div>
+                    </form>
+                    <ul className="nav navbar-nav navbar-right">
+                        <li><Link to="settings">Settings</Link></li>
+                        <li><Link to="about">About</Link></li>
+                    </ul>
+                    </div>
+                </div>
+                </nav>
         );
     }
 }
