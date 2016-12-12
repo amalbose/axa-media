3155b089a46b4df50cdc576d3ae1662b34f51050
axatrikx <axatrikx@gmail.com>
2016-12-12 21:48:04 +0530

    show rating only if present

--- src/js/components/Media.js
+++ src/js/components/Media.js
@@ -11,13 +11,17 @@ export default class Media extends React.Component {
         } else if(count == 4) {
             classVal = "mediaItem col-sm-3 col-lg-3 col-md-3";
         }
+        let rating = '';
+        if(mediaItem.imdbRating) {
+            rating = <p className="imdbRating">{mediaItem.imdbRating}</p> 
+        }
         return (
             <div className={classVal}>
                 <div className="thumbnail thumbnailCol">
+                    {rating}
                     <img src={mediaItem.poster} alt={mediaItem.processedFileName} />
                     <div className="caption">
                         <h5 className="noOverflow" title={mediaItem.processedFileName}>{mediaItem.processedFileName}</h5>
-                      <p>{mediaItem.imdbRating}</p> 
                      {/*   <p><a href="#" className="btn btn-primary" role="button">Button</a> <a href="#" className="btn btn-default" role="button">Button</a></p>
                         */}
                     </div>
